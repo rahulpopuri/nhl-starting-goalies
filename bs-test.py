2d2e1e74761a0f53fbb9685c9ba037b698ce0516
rahul <rahulpopuri@gmail.com>
2017-01-04 22:40:42 -0500

    Initial test of website scraper
    Also added slack code

---
+++ bs-test.py
@@ -0,0 +1,20 @@
+from BeautifulSoup import BeautifulSoup
+Soup = BeautifulSoup
+import BeautifulSoup
+import urllib2
+
+from slacker import Slacker
+
+slack = Slacker('xoxb-123655594677-nYOuf2MpUypZaVCIpVNhWdUF')
+
+STATUSES = ('Confirmed','Likely','Unconfirmed')
+
+page = urllib2.urlopen('http://www2.dailyfaceoff.com/starting-goalies/').read()
+soup = Soup(page)
+
+for g_a in soup.findAll("div", {"class" : ["goalie away", "goalie home"]}):
+    for name in g_a.findAll("h5"):
+    	goalie_name = name.text
+    for status in g_a.findAll("dl"):
+    	goalie_status = [s for s in STATUSES if s in status.text][0]
+    slack.chat.post_message('#general', goalie_name + ":" + goalie_status)
